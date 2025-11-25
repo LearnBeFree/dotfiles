162b23b2685fc8187130e905dd7deb83690025b4
opensource-lover <9l.schastliv@gmail.com>
2025-11-25 21:47:08 +0300

    modified keybinds

--- .config/nvim/lua/config/keybinds.lua
+++ .config/nvim/lua/config/keybinds.lua
@@ -1,3 +1,5 @@
 -- Setting a global leader key; space is a common practice
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>cd", vim.cmd.Ex)
+-- TODO i want <leader>do be written here, not dl
+vim.keymap.set('n', '<leader>dl', '<cmd>lua vim.diagnostic.open_float()<CR>', { noremap = true, silent = true })
